11907f7a88a3c53ceb69584e893260007eb84966
cuspcomputers@gmail.com <stirlingstout>
2023-12-19 10:28:11 +0000

    Added another test for procedures

--- HLA_VB_Tests/UnitTest_CALL_RETURN.vb
+++ HLA_VB_Tests/UnitTest_CALL_RETURN.vb
@@ -27,6 +27,31 @@ Namespace HLA_VB_Tests
             Assert.AreEqual(456, r(1))
         End Sub
 
+        <TestMethod>
+        Sub TestCALLRETURN2()
+            Dim m As Memory
+            Dim r As Registers
+            Dim errors As List(Of String)
+
+            Dim program = New List(Of String)() From {"CALL p", "HALT",
+                                                      "PROCEDURE P",
+                                                      "R1 = 456",
+                                                      "Call Q",
+                                                      "END PROCEDURE",
+                                                      "procedure Q",
+                                                      "R2 = R1 + 1",
+                                                      "end procedure"}
+            With CompileHLA(program)
+                m = .assembly
+                r = .registers
+                errors = .errorList
+            End With
+            Assert.AreEqual(0, errors.Count)
+            ExecuteProgram(m, r)
+
+            Assert.AreEqual(456, r(1))
+            Assert.AreEqual(457, r(2))
+        End Sub
     End Class
 
 End Namespace
